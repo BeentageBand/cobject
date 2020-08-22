000264fe131667c905fcbf2b792d0dffb3b3d585
BeentageBand <josera.baez@gmail.com>
2020-08-22 13:20:06 -0700

    Change TEMPLATE to EVAL3

--- cobject/ctemplate.h
+++ cobject/ctemplate.h
@@ -3,7 +3,7 @@
 
 #include "xmac.h"
 
-#define TEMPLATE(...) EVAL4(WHILE(CHECK, TEMPLATE_CAT, __VA_ARGS__, dummy))
+#define TEMPLATE(...) EVAL3(WHILE(CHECK, TEMPLATE_CAT, __VA_ARGS__, dummy))
 #define TEMPLATE_CAT(a, ...) CAT(a, IF( CHECK(__VA_ARGS__) )(CAT(_, __VA_ARGS__), ) )
 
 #define Member_Name(...) TEMPLATE(__VA_ARGS__)
@@ -22,6 +22,6 @@
 
 #define INDIRECT_SELECT() SELECT_PARAM
 
-#define T_Param(num, ...) EVAL4(SELECT_PARAM(num, __VA_ARGS__))
+#define T_Param(num, ...) EVAL5(SELECT_PARAM(num, __VA_ARGS__))
 
 #endif /*CTEMPLATE_H_*/
